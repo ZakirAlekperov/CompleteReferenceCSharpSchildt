8b3ad941fa29f6edb28734681bbb7ddb188b11f0
zakir <zakir@ZAKPC>
2023-09-14 21:26:33 +0300

    Add project files.

---
+++ CompleteReferenceCSharpSchildt/Chapter2/Project1.cs
@@ -0,0 +1,12 @@
+﻿/*
+ * Это простая программа на С#
+ */
+
+class Example
+{
+    //Любая программа на C# начинается с метода Main()
+    static void Main()
+    {
+        System.Console.WriteLine("Простая программа на C#");
+    }
+}
